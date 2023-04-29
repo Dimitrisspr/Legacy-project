c4deb192a52bd809d19744b5a821906d103d98d2
Dimitris Spyropoulos <dimitris.spyropoulos90@gmail.com>
2023-04-29 12:39:56 +0300

    final

--- MemoGallery-main/client/src/components/protectedComponents/GalleryForm/GalleryForm.js
+++ MemoGallery-main/client/src/components/protectedComponents/GalleryForm/GalleryForm.js
@@ -1,8 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "./GalleryForm.css";
 import axios from "axios";
-import {useNavigate} from "react-router-dom"
-
+import { useNavigate } from "react-router-dom";
 
 function GalleryForm() {
   const [image, setImage] = useState("");
@@ -10,7 +9,7 @@ function GalleryForm() {
   const [file, setFile] = useState();
   const navigate = useNavigate();
   /* using useRef hook to clean the input after adding the photo */
-  const inputRef = useRef(null)
+  const inputRef = useRef(null);
 
   const uploadImage = (e) => {
     setFile(URL.createObjectURL(e.target.files[0]));
@@ -23,39 +22,52 @@ function GalleryForm() {
       photoUrl: image,
       description: description,
     };
-    
+
     const token = localStorage.getItem("token");
     await axios
-      .post("http://localhost:8080/auth/postPhotos", newPhoto,{
-        headers:{
-            'Authorization': `Bearer ${token}`
-        }
-    })
+      .post("http://localhost:8080/auth/postPhotos", newPhoto, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
       .then((res) => alert(res.data));
-      navigate("/postPhotos")
+    navigate("/postPhotos");
   }
 
-  const clearInput = ()=>{
-    inputRef.current.value = ""
-  }
+  const clearInput = () => {
+    inputRef.current.value = "";
+  };
 
-    return ( 
-        <div className="main"> 
-            <form onSubmit={addPhoto}>
-                <div className="imageSection">
-                    <label>Insert Image</label>
-                    <input type="file" accept="images/*" onChange={uploadImage}/>
-                    <img src={file} style={{width:100,height:100}}/>
-                    <input type="text" placeholder="you can insert image url" value={image} onChange={(e)=>setImage(e.target.value)} ref={inputRef}/>       
-                </div>        
-                <br/>
-                <label>What is your memory: </label>
-                <textarea type="text" value={description} onChange={(e)=>setDescription(e.target.value)} ref={inputRef}/>
-                <br/>
-                <button type="submit" onClick={clearInput}>Add to Gallery</button>
-            </form>
+  return (
+    <div className="main">
+      <form onSubmit={addPhoto}>
+        <div className="imageSection">
+          <label>Insert Image</label>
+          <input type="file" accept="images/*" onChange={uploadImage} />
+          <img src={file} style={{ width: 100, height: 100 }} />
+          <input
+            type="text"
+            placeholder="you can insert image url"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            ref={inputRef}
+          />
         </div>
-     );
+        <br />
+        <label>What is your memory: </label>
+        <textarea
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          ref={inputRef}
+        />
+        <br />
+        <button type="submit" onClick={clearInput}>
+          Add to Gallery
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default GalleryForm;
