c65ef3491b20ada6bd168f4295a42d80208c2907
Dimitris Spyropoulos <dimitris.spyropoulos90@gmail.com>
2023-06-11 19:09:03 +0300

    build

--- MemoGallery-main/client/src/components/freeComponents/LoginForm/PostLoginData.js
+++ MemoGallery-main/client/src/components/freeComponents/LoginForm/PostLoginData.js
@@ -1,9 +1,12 @@
 import axios from "axios";
+import API_URL from "../../config"
 
 const PostLoginData = async (data) => {
  
   try {
-    const response = await axios.post("http://localhost:8080/login", data);
+    //const response = await axios.post("http://localhost:8080/login", data);
+    const response = await axios.post(`${API_URL}/login`, data);
+
     console.log(response);
     if (response.status === 200) {
       localStorage.setItem("token", response.data);
--- MemoGallery-main/client/src/components/freeComponents/RegistrationForm/PostRegistData.js
+++ MemoGallery-main/client/src/components/freeComponents/RegistrationForm/PostRegistData.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import API_URL from "../../config"
 
 const PostRegistData = async (data) => {
   try {
-    await axios.post("http://localhost:8080/register", data);
+    await axios.post(`${API_URL}/register`, data);
   } catch (error) {
     alert("Unable to Register...");
     console.log(error);
